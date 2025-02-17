6c70fdaf8ac42486b4fe22334847295451b75498
idekwat <184945414+idekwat@users.noreply.github.com>
2025-02-17 18:44:58 +0800

    added simple layouts, added functions to add new books, basic outlines for styling

--- scripts.js
+++ scripts.js
@@ -2,20 +2,32 @@
 const myLibrary = [];
 const newBook = document.getElementById("addButton");
 const bookForm = document.getElementById("newBook");
+const submitData = document.getElementById("submitButton");
+const bookColumn = document.querySelector(".bookList");
 
-
-function Book() {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book(title, author, pages) { //read
+    const book = new Array([title, author, pages]); //read
+    myLibrary.push(book);
+    console.log(myLibrary);
+    bookLibrary(myLibrary);
 }
 
 function addBook() {
     bookForm.showModal();
 }
-/*  
-this.reportBook = function() {
-let info = title + "by " + author + ", " + pages + " pages, " + read + ".";
-return info;
-}*/
+
+submitData.onclick = (e) => {
+    e.preventDefault();
+    let title = document.getElementById("bookTitle").value;
+    let author = document.getElementById("bookAuthor").value;
+    let pages = document.getElementById("pageTotal").value;
+   // let read = document.getElementById("readStatus").value;
+    Book(title, author, pages)//, read);
+}
+
+function bookLibrary(currentbooks) {    
+    const displayBook = document.createElement("div");
+    displayBook.textContent = currentbooks.map((book) => book.title + " S P A C E ");
+    bookColumn.appendChild(displayBook);
+}
+
